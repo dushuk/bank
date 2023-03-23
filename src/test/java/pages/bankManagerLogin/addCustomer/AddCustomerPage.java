2cf4159525c17b4f87ec7a66593245997d638749
Dushuk Oleksandr <dushuk@gmail.com>
2023-03-23 11:25:51 +0100

    Described pages AddCustomerPage, OpenAccountPage

--- src/test/java/pages/bankManagerLogin/addCustomer/AddCustomerPage.java
+++ src/test/java/pages/bankManagerLogin/addCustomer/AddCustomerPage.java
@@ -1,10 +1,41 @@
 package pages.bankManagerLogin.addCustomer;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.PageBase;
+import wait.Wait;
 
 public class AddCustomerPage extends PageBase {
     public AddCustomerPage(WebDriver driver) {
         super(driver);
+    }
+
+    Wait wait;
+    @FindBy(xpath = "input[@placeholder='First Name']")
+    protected WebElement firstNameField;
+    @FindBy(xpath = "input[@placeholder='Last Name']")
+    protected WebElement lastNameField;
+    @FindBy(xpath = "input[@placeholder='Post Code']")
+    protected WebElement postCodeField;
+    @FindBy(xpath = "//button[@type='submit']")
+    protected WebElement addCustomerButton;
+
+    public void waitForLoading() {
+        wait = new Wait(driver);
+        wait.forVisibility(firstNameField);
+        wait.forVisibility(lastNameField);
+        wait.forVisibility(postCodeField);
+        wait.forVisibility(addCustomerButton);
+    }
+
+    public void fillAddCustomerForm(String firstName, String lastName, String postCode) {
+        fillField(firstNameField, firstName);
+        fillField(lastNameField, lastName);
+        fillField(postCodeField, postCode);
+    }
+
+    public void clickOnAddCustomerButton() {
+        click(addCustomerButton);
     }
 }
--- src/test/java/pages/bankManagerLogin/openAccount/OpenAccountPage.java
+++ src/test/java/pages/bankManagerLogin/openAccount/OpenAccountPage.java
@@ -1,10 +1,40 @@
 package pages.bankManagerLogin.openAccount;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import pages.PageBase;
+import wait.Wait;
 
 public class OpenAccountPage extends PageBase {
     public OpenAccountPage(WebDriver driver) {
         super(driver);
+    }
+
+    Wait wait;
+    @FindBy(xpath = "//select[@id='userSelect']")
+    protected WebElement customerNameDropdown;
+    @FindBy(xpath = "//select[@id='currency']")
+    protected WebElement currencyDropdown;
+    @FindBy(xpath = "//button[@type='submit']")
+    protected WebElement processButton;
+
+    public void waitForLoading() {
+        wait = new Wait(driver);
+        wait.forVisibility(customerNameDropdown);
+        wait.forVisibility(currencyDropdown);
+        wait.forVisibility(processButton);
+    }
+
+    public void selectExistingUser(String userName) {
+        selectOption(userName, customerNameDropdown);
+    }
+
+    public void selectCurrency(String currency) {
+        selectOption(currency, currencyDropdown);
+    }
+
+    public void clickOnButton() {
+        click(processButton);
     }
 }
